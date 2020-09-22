4fdd8f3d1eb6ca15a16d1a08b698cc20cfc1f7a6
Stefan Merker <stefanmerker@googlemail.com>
2020-09-22 10:00:48 +0200

    Eingabe Stellenanzahl

---
+++ PE_palindrome_product/PE_palindrome_product/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PE_palindrome_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int laenge;
+
+            Console.WriteLine("Länge eingeben:");
+            laenge = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            string set = "";
+
+            while (i < laenge)
+            {
+                set = set + "9";
+                i++;
+            }
+
+            int a = Convert.ToInt32(set);
+            int b = Convert.ToInt32(set);
+
+            int product = a * b;
+
+            Console.WriteLine(a + " * " + b + " = " + product);
+            Console.ReadKey();
+        }
+    }
+}
